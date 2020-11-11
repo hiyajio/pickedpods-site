80d6e2b6765338172a0b0d6274125f8d32a91c27
lockjio <jio.buenviaje@gmail.com>
2020-11-10 21:07:05 -0500

    Convert all jquery in main to vanilla js

--- jsfrontend/js/main.js
+++ jsfrontend/js/main.js
@@ -15,62 +15,29 @@ let deleteButton = document.querySelector("#delete-button");
 addButton.onmouseup = addURLFromAPI;
 deleteButton.onmouseup = deleteURLFromAPI;
 
-let rateValue = $("#qty_input").val();
+let rateValue = document.querySelector("#qty_input").value;
 
-jQuery(document).ready(function ($) {
-	"use strict";
-
+window.onload = function init() {
 	httpStart();
+	var input = document.getElementById("qty_input");
+	input.addEventListener("change", updateCount, false);
+};
 
-	$("#qty_input").change(function () {
-		rateValue = $(this).val();
-		document.getElementById("qty_input").value = rateValue;
+function updateCount(event) {
+	rateValue = event.target.value;
+	document.getElementById("qty_input").value = rateValue;
 
-		var listCount = document.getElementsByClassName("pb-embed");
+	var listCount = document.getElementsByClassName("pb-embed");
 
-		for (var j = 1; j <= listCount.length; j++) {
-			document
-				.getElementById("podcast-inner" + j)
-				.setAttribute("data-limit", rateValue);
-		}
+	for (var j = 1; j <= listCount.length; j++) {
+		document
+			.getElementById("podcast-inner" + j)
+			.setAttribute("data-limit", rateValue);
+	}
 
-		clearApp();
-		httpStart();
-	});
-
-	var siteMagnificPopup = function () {
-		$(".image-popup").magnificPopup({
-			type: "image",
-			closeOnContentClick: true,
-			closeBtnInside: false,
-			fixedContentPos: true,
-			mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				verticalFit: true,
-			},
-			zoom: {
-				enabled: true,
-				duration: 300, // don't foget to change the duration also in CSS
-			},
-		});
-
-		$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
-			disableOn: 700,
-			type: "iframe",
-			mainClass: "mfp-fade",
-			removalDelay: 160,
-			preloader: false,
-
-			fixedContentPos: false,
-		});
-	};
-	siteMagnificPopup();
-});
+	clearApp();
+	httpStart();
+}
 
 function clearApp() {
 	var xmlHttp = new XMLHttpRequest();
